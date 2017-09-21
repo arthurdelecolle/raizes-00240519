a7e5f868499c2371d2070a76e2a4722b3eecc691
00240519 <31625435+00240519@users.noreply.github.com>
2017-09-21 19:53:10 -0300

    Update ufrgs_bissecao.m

--- ufrgs_bissecao.m
+++ ufrgs_bissecao.m
@@ -1,5 +1,25 @@
-function p=ufrgs_bissecao(f,a,b,N)
-% Metodo da Bissecao
-%
-% p=ufrgs_bissecao(f,a,b,N)
+function p=ufrgs_bissecao(f,a,b,e1,N)
 
+%declarar f usando f=@(x)
+%declarar a e b (intervalos da função)
+%declarar e1 a margem para resposta certa
+%declarar N sendo número de iterações
+
+%OK
+
+for i=1:N
+    p=((a+b)/2);
+    
+    if abs((b-a)/2)<=e1;
+    break
+    end 
+       
+        if f(a)*f(p)<=0
+        b=p;
+        
+        else
+        a=p;
+        end
+    
+
+end
