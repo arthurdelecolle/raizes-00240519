5caaa4fe07f473f699ab0f12fdc0dc25aa15d663
00240519 <31625435+00240519@users.noreply.github.com>
2017-09-21 20:27:00 -0300

    Update ufrgs_pfalsa.m

--- ufrgs_pfalsa.m
+++ ufrgs_pfalsa.m
@@ -1,4 +1,27 @@
 function p=ufrgs_pfalsa(f,a,b,N,e1,e2,e3)
-% Metodo da Posicao Falsa
-%
-% p=ufrgs_pfalsa(f,a,b,N,e1,e2,e3)
+
+for i=1:N
+    
+    p=(a*f(b)-b*f(a))/(f(b)-f(a));
+    
+    if abs(f(p))<=e1; 
+    break
+    end    
+        
+    if abs(b-a)<=e2; 
+    break
+    end
+    
+    if abs((b-a)/b)<=e3; 
+    break
+    end  
+    
+        if f(a)*f(p)<0
+        b=p;
+        
+        else
+        a=p;
+        end
+   
+
+end
