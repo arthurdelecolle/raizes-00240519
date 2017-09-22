82172497ba3f594522bfcddc302165da0d4f99ca
00240519 <31625435+00240519@users.noreply.github.com>
2017-09-22 13:20:15 -0300

    Update ufrgs_newton.m

--- ufrgs_newton.m
+++ ufrgs_newton.m
@@ -2,21 +2,20 @@ function xn1=ufrgs_newton(f,df,xn,N,e1,e2,e3)
 % Metodo de Newton
 %
 % xn1=ufrgs_newton(f,df,xn,N,e1,e2,e3)
+
 for i=1:N
-    xn1=(xn*f(xnm1)-xnm1*f(xn))/(f(xnm1)-f(xn));
+    xn1=xn-(f(xn)/df(xn));
     
-    if abs (f(xn1)) <=e1
+     if abs(f(xn1))<=e1
         break
-    end    
+    end 
     if abs((xn1-xn))<=e2  
         break 
     end
-    if abs((xn1-xn)/xn1)<=e3  
+    if abs((xn1-xn)/xn1)<=e3 
         break 
-    end
-        
-   
-        xnm1=xn;
+    end 
+           
         xn=xn1;
     end
    
